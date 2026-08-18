733f1042808722b0f575224a8aefba44a7a65b44
Chitransh27309-crypto <ayush27309@gmail.com>
2026-08-18 23:24:26 +0530

    Fix backend environment configuration

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: './backend/.env' });
+require('dotenv').config({
+  path: require('path').join(__dirname, '.env')
+})
 const connectToMongo = require('./db')
 const express = require('express')
 var cors = require('cors');
